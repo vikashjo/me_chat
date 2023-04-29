6cfe53ff6d67c4c2468dd7dac6b3addfc5bb30a5
vikashjo <vikashjoshi431@gmail.com>
2023-04-29 11:32:30 +0530

    Adding routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'sessions/index'
+	root 'chatroom#index'
+	get 'login', to: 'session#index'
 end
